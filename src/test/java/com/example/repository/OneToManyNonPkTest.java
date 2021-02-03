5ee205592d9ebd57024255dd1b969f981adfd96c
LeeJunGyun <rainmaker0303@hanmail.net>
2021-02-03 19:35:57 +0900

    데이터 생성안되는거 처리

--- src/test/java/com/example/repository/OneToManyNonPkTest.java
+++ src/test/java/com/example/repository/OneToManyNonPkTest.java
@@ -3,6 +3,7 @@ package com.example.repository;
 
 import com.example.domain.ADomain;
 import com.example.domain.BDomain;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.is;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OneToManyNonPkTest {
@@ -24,9 +27,10 @@ public class OneToManyNonPkTest {
     @Autowired
     private BDomainRepository bDomainRepository;
 
+    private String testKey = "test";
+
     @Before
     public void setUp() throws Exception {
-        String testKey = "test";
         BDomain bDomain = new BDomain();
         bDomain.setTestKey(testKey);
         bDomainRepository.save(bDomain);
@@ -46,8 +50,12 @@ public class OneToManyNonPkTest {
     public void OneToMany_NON_PK_조회_테스트() throws Exception {
         List<ADomain> aDomains = aDomainRepository.findAll();
         List<BDomain> bDomains = aDomains.get(0).getB();
+
         bDomains.forEach(domain -> {
             System.out.println(domain);
         });
+
+//        Assert.assertThat(bDomains.get(0).getTestKey(), is(testKey));
+//        Assert.assertThat(bDomains.get(1).getTestKey(), is(testKey));
     }
 }
